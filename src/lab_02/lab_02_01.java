17ce245fb4ea58fa8a6f0a091e68e8e9def1b30d
BinhLe24 <lemaithaobinh2@gmail.com>
2022-09-09 11:18:57 +0700

    Binh push lab 02

---
+++ src/lab_02/lab_02_01.java
@@ -0,0 +1,26 @@
+package lab_02;
+
+import java.sql.SQLOutput;
+import java.util.Scanner;
+
+public class lab_02_01 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Please enter your weight(kg):");
+        float clientWeight = scanner.nextFloat();
+        System.out.print("Please enter your height(m):");
+        float clientHeight = scanner.nextFloat();
+
+        float BMI = clientWeight / (clientHeight * 2);
+        System.out.println("BMI = " + BMI);
+        if (BMI<18.5){
+            System.out.println("Underweight");
+        } else if (BMI<=24.9) {
+            System.out.println("Normal weight");
+        } else if (BMI<=29.9) {
+            System.out.println("Overweight");
+        } else {
+            System.out.println("Obesity");
+        }
+    }
+}
