d2ae7c62b7a4670a2fb1dfec6eb3520a67326dc0
BinhLe24 <lemaithaobinh2@gmail.com>
2022-09-09 14:15:28 +0700

    Binh push code lab02_02

--- src/lab_02/lab_02_02.java
+++ src/lab_02/lab_02_02.java
@@ -1,7 +1,20 @@
 package lab_02;
 
+import java.util.Scanner;
+
 public class lab_02_02 {
     public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Please enter your number: ");
+        long myNumber = scanner.nextLong();
 
+        int soDu = (int) myNumber%2;
+        if (myNumber==0){
+            System.out.println("It is neither an even number nor odd number");
+        } else if(soDu == 0) {
+            System.out.println("It is an even number");
+        } else {
+            System.out.println("--> It is an odd number");
+        }
     }
 }
